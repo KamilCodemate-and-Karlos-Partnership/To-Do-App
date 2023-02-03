eaea5a4ecc6d007739ab28b38e3e131f5cefaa99
Kamil Marczak <kamil.codemate@gmail.com>
2023-02-03 23:26:40 +0100

    Add client

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,7 +2,7 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
   res.json({ status: 'OK', data: 'randomData' });
 });
 app.listen(5000);
