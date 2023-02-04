53c0125f95ceec142a69614acdf6d4ebdbd19a59
Karlos <grze.piluk@gmail.com>
2023-02-04 16:59:45 +0100

    Added handlers to SignInForm

--- client/src/pages/SignInForm.tsx
+++ client/src/pages/SignInForm.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import FormHeader from '../components/Forms/FormHeader';
 import FormContainer from '../components/Forms/FormContainer';
 import FormContent from '../components/Forms/FormContent';
@@ -7,32 +7,57 @@ import signinimage from '../assets/img/signinimage.jpg';
 import '../assets/styles/SignUpForm.scss';
 
 const SignInForm: React.FC<{}> = (): React.ReactElement => {
+  const [inputValues, setInputValues] = useState({
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValues({
+      ...inputValues,
+      [e.target.name]: e.target.value,
+    });
+    e.preventDefault();
+    console.log([e.target.name], e.target.value);
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
+    e.preventDefault();
+    console.log('Handle Submit');
+  };
+
   return (
     <div className="SignInForm">
-      {/* <FormContainer>
-      <>
-        <FormHeader title="Sign In" />
-        <div className="form-main">
-          <FormContent>
-            <form method="post">
-              <div className="form-main-title">
-                <h2>Hello!</h2>
-                <p>Please sign in to continue</p>
-              </div>
-              <label>Email Address</label>
-              <input type="email" />
-              <label>Password</label>
-              <input type="password" />
-              <input type="submit" value="Sign In" />
-            </form>
-          </FormContent>
-          <FormSidebar img={signinimage}>
-            <p>You're not <span>sign up</span> yet?</p>
-            <button>Sign Up</button>
-          </FormSidebar>
-        </div>
-      </>
-      </FormContainer> */}
+      <FormContainer>
+        <>
+          <FormHeader title="Sign In" />
+          <div className="form-main">
+            <FormContent>
+              <form onSubmit={handleSubmit}>
+                <div className="form-main-title">
+                  <h2>Hello!</h2>
+                  <p>Please sign in to continue</p>
+                </div>
+                <label>Email Address</label>
+                <input type="email" name="email" onChange={handleChange} />
+                <label>Password</label>
+                <input
+                  type="password"
+                  name="password"
+                  onChange={handleChange}
+                />
+                <input type="submit" value="Sign In" />
+              </form>
+            </FormContent>
+            <FormSidebar img={signinimage}>
+              <p>
+                You're not <span>sign up</span> yet?
+              </p>
+              <button>Sign Up</button>
+            </FormSidebar>
+          </div>
+        </>
+      </FormContainer>
     </div>
   );
 };
--- client/src/pages/SignUpForm.tsx
+++ client/src/pages/SignUpForm.tsx
@@ -36,32 +36,42 @@ const SignUpForm: React.FC<{}> = (): React.ReactElement => {
   return (
     <div className="SignUpForm">
       <FormContainer>
-      <>
-        <FormHeader title="Sign Up" />
-        <div className="form-main">
-          <FormContent>
-            <form onSubmit={handleSubmit}>
-              <div className="form-main-title">
-                <h2>Hello!</h2>
-                <p>Please sign up to continue</p>
-              </div>
-              <label>Full Name</label>
-              <input type='text' name='fullName' onChange={handleChange} />
-              <label>Email Address</label>
-              <input type='email' name='email' onChange={handleChange} />
-              <label>Password</label>
-              <input type='password' name='password' onChange={handleChange} />
-              <label>Confirm Password</label>
-              <input type='password' name='passwordConfirm' onChange={handleChange} />
-              <input type='submit' value='Sign Up' />
-            </form>
-          </FormContent>
-          <FormSidebar img={signupimage}>
-          <p>I'm already a <span>member</span>!</p>
-          <button>Sign In</button>
-        </FormSidebar>
-        </div>
-      </>
+        <>
+          <FormHeader title="Sign Up" />
+          <div className="form-main">
+            <FormContent>
+              <form onSubmit={handleSubmit}>
+                <div className="form-main-title">
+                  <h2>Hello!</h2>
+                  <p>Please sign up to continue</p>
+                </div>
+                <label>Full Name</label>
+                <input type="text" name="fullName" onChange={handleChange} />
+                <label>Email Address</label>
+                <input type="email" name="email" onChange={handleChange} />
+                <label>Password</label>
+                <input
+                  type="password"
+                  name="password"
+                  onChange={handleChange}
+                />
+                <label>Confirm Password</label>
+                <input
+                  type="password"
+                  name="passwordConfirm"
+                  onChange={handleChange}
+                />
+                <input type="submit" value="Sign Up" />
+              </form>
+            </FormContent>
+            <FormSidebar img={signupimage}>
+              <p>
+                I'm already a <span>member</span>!
+              </p>
+              <button>Sign In</button>
+            </FormSidebar>
+          </div>
+        </>
       </FormContainer>
     </div>
   );
