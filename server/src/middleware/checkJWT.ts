3b190e0d9b9a87ff343a3c80ce8802f193a1587a
Kamil Marczak <kamil.codemate@gmail.com>
2023-02-09 20:37:27 +0100

    Add login redirect and jwt save

--- server/src/middleware/checkJWT.ts
+++ server/src/middleware/checkJWT.ts
@@ -3,15 +3,14 @@ import { verify } from 'jsonwebtoken';
 import { config } from 'dotenv';
 const app = express();
 
-export default function checkJWT(req: Request, res: Response, next: NextFunction): Promise<Object> {
-  return new Promise((resolve, reject) => {
-    const authorizationHeader = req.headers['authorization'];
-    const authToken = authorizationHeader && authorizationHeader.split(' ')[1];
-    if (!authToken) reject({ success: false, errorContent: 'Missing authorization token' });
+export default function checkJWT(req: Request, res: Response, next: NextFunction): any {
+  const authorizationHeader = req.headers['authorization'];
+  const authToken = authorizationHeader && authorizationHeader.split(' ')[1];
+  if (!authToken) return res.status(401).json({ success: false, errorContent: 'Missing authorization token' });
 
-    verify(authToken as string, process.env.ACCESS_TOKEN_SECRET as string, (err, user) => {
-      if (err) reject({ success: false, errorContent: 'invalid auth token' });
-      (req as any).user = user;
-    });
+  verify(authToken as string, process.env.ACCESS_TOKEN_SECRET as string, (err, user) => {
+    if (err) return res.status(401).json({ success: false, errorContent: 'invalid authorization token' });
+    (req as any).user = user;
+    next();
   });
 }
